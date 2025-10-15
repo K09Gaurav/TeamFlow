63e967626c70f1738082245d68033b284f96e583
Kumar Gaurav <k09gaurav@gmail.com>
2025-10-15 13:46:43 +0530

    init

---
+++ src/test/java/com/k09gaurav/teamflow/TeamFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k09gaurav.teamflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
